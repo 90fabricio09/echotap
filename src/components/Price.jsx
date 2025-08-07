26f083fd9f367e9ad23ad6f867fe975a78654a18
90fabricio09 <90934129+90fabricio09@users.noreply.github.com>
2025-08-07 08:55:17 -0300

    Update Price.jsx

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -30,7 +30,7 @@ const Price = () => {
                             </div>
                             <h3 className="pricing-plan">EchoTap</h3>
                             <div className="pricing-price">
-                                <span className="price">R$ 79,90</span>
+                                <span className="price">R$ 49,90</span>
                                 <span className="period">Valor no PIX</span>
                             </div>
                             <ul className="pricing-features">
@@ -54,7 +54,7 @@ const Price = () => {
                             </div>
                             <h3 className="pricing-plan">EchoTap Personalizado</h3>
                             <div className="pricing-price">
-                                <span className="price">R$ 139,90</span>
+                                <span className="price">R$ 79,90</span>
                                 <span className="period">Valor no PIX</span>
                             </div>
                             <ul className="pricing-features">
@@ -77,3 +77,4 @@ const Price = () => {
 };
 
 export default Price; 
+
