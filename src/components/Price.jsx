6f11789e6e35d5481ec361e699ff0022d4cf18f8
90fabricio09 <90934129+90fabricio09@users.noreply.github.com>
2025-08-19 09:42:59 -0300

    Update Price.jsx

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -30,7 +30,7 @@ const Price = () => {
                             </div>
                             <h3 className="pricing-plan">EchoTap</h3>
                             <div className="pricing-price">
-                                <span className="price">R$ 49,90</span>
+                                <span className="price">R$ 59,90</span>
                                 <span className="period">Valor no PIX</span>
                             </div>
                             <ul className="pricing-features">
@@ -77,4 +77,5 @@ const Price = () => {
 };
 
 export default Price; 
+
 
